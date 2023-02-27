829db19a2d3abae82cc42a4613640aed861845ef
kawuh <hakeemyunus03@gmail.com>
2023-02-27 08:05:16 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,16 +2,15 @@
 
 /**
  * print_alphabet - print all alphabet in lowercase
- *
- * Return: Always 0.
  */
 
-void print_alphabet("a-z")
+void print_alphabet(void)
 {
 	char letter;
 
-	for(letter = 'a'; letter <= 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
+
 	_putchar('\n');
-	return(0);
 }
+

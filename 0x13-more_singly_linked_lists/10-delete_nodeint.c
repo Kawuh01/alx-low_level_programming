767ce78f23e3dfab3affc0cf1d046ef198d23c5b
daweharck <hakeemyunus03@gmail.com>
2023-04-03 12:35:52 -0700

    delete a node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -7,7 +7,7 @@
  *
  * Return: 1 (Success), or -1 (Fail)
  */
-int delete_nodeint_at_index(listint_t **head, unsigned int index)p
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *temp = *head;
 	listint_t *current = NULL;
@@ -22,6 +22,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)p
 		free(temp);
 		return (1);
 	}
+
 	while (i < index - 1)
 	{
 		if (!temp || !(temp->next))
